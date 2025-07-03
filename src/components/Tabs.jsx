9fb245e0146bcaa5fc51d7d1d35d795f339947d7
aradhita2001 <aradhita2001@gmail.com>
2025-07-03 16:30:37 +0530

    final commit

---
+++ src/components/Tabs.jsx
@@ -0,0 +1,9 @@
+export default function Tabs({buttons, ButtonsContainer="menu", children}){
+
+    return (
+        <>
+            <ButtonsContainer>{buttons}</ButtonsContainer>
+            {children}
+        </>
+    )
+}
